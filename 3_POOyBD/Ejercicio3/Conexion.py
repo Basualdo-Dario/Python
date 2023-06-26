fc7b6bc58f0ee82adc54f8ef6333edbbf69568c0
Dario Basualdo <basualdodario057@gmail.com>
2023-06-26 17:27:29 -0300

    o

--- 3_POOyBD/Ejercicio3/Conexion.py
+++ 3_POOyBD/Ejercicio3/Conexion.py
@@ -1,15 +1,14 @@
-from logger_base import log 
 from psycopg2 import pool
+from logger_base import log 
 import sys
 
-
 class Conexion:
-    _DATABASE ="test_db"
-    _USERNAME ="postgres"
-    _PASSWORD ="admin"
-    _PORT ="5432"
-    _HOST ="127.0.0.1"
-    _MINCON = 1
+    _DATABASE ='test_db'
+    _USERNAME = 'postgres'
+    _PASSWORD = 'admin'
+    _PORT = '5432'
+    _HOST= '127.0.0.1'
+    _MINCON =1
     _MAXCON = 5
     _pool = None
 
@@ -19,35 +18,62 @@ class Conexion:
         if cls._pool is None:
             try:
                 cls._pool = pool.SimpleConnectionPool(cls._MINCON,cls._MAXCON,
-                                                     host = cls._HOST,
-                                                     user = cls._USERNAME,
-                                                     password = cls._PASSWORD,
-                                                     port = cls._PORT,
-                                                     database = cls._DATABASE)
-                
-                log.debug(f'Creacion del pool exitosa: {cls._pool}')
+                                              host = cls._HOST,
+                                              user = cls._USERNAME,
+                                              password = cls._PASSWORD,
+                                              port = cls._PORT,
+                                              database = cls._DATABASE)
+                log.debug(f'Pool obtenido {cls._pool}')
                 return cls._pool
-            
             except Exception as e:
-                log.error(f'Ocurrio un error al obtener el pool: {e}')
-                sys.exit
-
+                log.debug(f'Error al obtener el pool{e}')
+                sys.exit()
+        else:
+            return cls._pool
+        
 
     @classmethod
     def obtenerConexion(cls):
         conexion = cls.obtenerPool().getconn()
-        log.debug(f'Conexion obtenida del pool: {conexion}')
+        log.debug(f'Conexion obtenida{conexion}')
         return conexion
     
     @classmethod
-    def liberarConexion(cls,Conexion):
-        cls.obtenerpool().putconn(Conexion)
-        log.debug(f'Regresamos la conexion al Pool: {Conexion}')
+    def liberarConexion(cls,conexion):
+        cls.obtenerPool().putconn(conexion)
+        log.debug(f'Conexion liberada{conexion}')
 
-    
+    @classmethod
+    def cerrarConexiones(cls):
+        cls.obtenerPool().closeall()
+        log.debug(f'Conexiones cerradas')
 
-if __name__ == '__main__':
-  
+
+
+
+if __name__ =='__main__':
+    conexion1 = Conexion.obtenerConexion()
+
+    Conexion.liberarConexion(conexion1)
+
+
     conexion2 = Conexion.obtenerConexion()
 
 
+    conexion3 = Conexion.obtenerConexion()
+
+    conexion4 = Conexion.obtenerConexion()
+
+    Conexion.liberarConexion(conexion3)
+
+    conexion5 = Conexion.obtenerConexion()
+
+    Conexion.liberarConexion(conexion4)
+
+    conexion6 = Conexion.obtenerConexion()
+
+    conexion7 = Conexion.obtenerConexion()
+
+    conexion8 = Conexion.obtenerConexion()
+  
+    Conexion.cerrarConexiones()
--- 3_POOyBD/Ejercicio3/logger_base.py
+++ 3_POOyBD/Ejercicio3/logger_base.py
@@ -1,17 +1,23 @@
-import logging as log 
+import logging as log
 
-log.basicConfig( level= log.DEBUG,
-                format='%(asctime)s : %(levelname)s [%(filename)s:%(lineno)s] %(message)s',
+log.basicConfig(level = log.DEBUG ,
+                format='%(asctime)s: %(levelname)s [%(filename)s:%(lineno)s] %(message)s',
                 datefmt='%I:%M:%S %p',
                 handlers=[
-                 log.FileHandler('control_usuario'),
-                 log.StreamHandler()      
+                    log.FileHandler('capa_data4.log'),
+                    log.StreamHandler()
                 ])
 
 
 if __name__ == '__main__':
-    log.debug('Mensaje a Nivel debug')
-    log.info('Mensaje a Nivel info')
-    log.warning('Mensaje a Nivel warning')
-    log.error('Mensaje a Nivel error')
-    log.critical('Mensaje a Nivel critical')
+
+    log.debug('debug')
+
+    log.info('info')
+
+    log.warning('warning')
+
+    log.error('error')
+
+    log.critical('critical')
+    
